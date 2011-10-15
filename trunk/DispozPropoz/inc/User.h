72a940b16e482ed72947b451694d836733406185
sathishvj@gmail.com <sathishvj@gmail.com@5a97ccac-b474-ded1-4a52-e8af14ae9110>
2011-10-15 13:27:17 +0000

    svj: added empty shell functions

--- trunk/DispozPropoz/inc/User.h
+++ trunk/DispozPropoz/inc/User.h
@@ -3,6 +3,7 @@
  *
  *  Created on: 15 Oct 2011
  *      Author: vj
+ *  svj: added member variables and empty functions
  */
 
 #ifndef USER_H_
@@ -19,13 +20,27 @@ public:
 	User();
 	virtual ~User();
 
-private:
+public:
+	//Update profile information.  Return false if there is an error.
+	bool UpdateProfileInfo(String name, String phoneNumber, bool showPhoneNumber,
+			String email, bool showEmail,
+			QualifiedCoordinates coordinates);
+
+	//Load the user information corresponding to this id.
+	//Usage: could be used to load the previous user who logged in on this one.
+	//Id information would have to be saved to disk prior to exit.
+	static User LoadUser(Integer id);
+
+//private:
+//svj: decided to make this public for now to avoid individual getters and setters.
+public:
 	String name;
 	String phoneNumber;
 	bool showPhoneNumber;
 	String email;
 	bool showEmail;
 	QualifiedCoordinates defaultCoordinates;
+	Integer id;
 };
 
 #endif /* USER_H_ */
