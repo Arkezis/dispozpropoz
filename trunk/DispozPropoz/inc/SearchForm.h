980ae7f606e8e377372949c961705b289c69b9bc
sathishvj@gmail.com <sathishvj@gmail.com@5a97ccac-b474-ded1-4a52-e8af14ae9110>
2011-10-15 15:10:12 +0000

    Initial version of class for the SearchForm.

---
+++ trunk/DispozPropoz/inc/SearchForm.h
@@ -0,0 +1,33 @@
+/*
+ * SearchForm.h
+ *
+ *  Created on: 15 Oct 2011
+ *      Author: vj
+ */
+
+#ifndef SEARCHFORM_H_
+#define SEARCHFORM_H_
+
+#include <FBase.h>
+#include <FUi.h>
+
+using namespace Osp::Base;
+using namespace Osp::Ui;
+using namespace Osp::Ui::Controls;
+
+class SearchForm  :
+	public Osp::Ui::Controls::Form,
+	public Osp::Ui::IActionEventListener {
+
+public:
+	SearchForm();
+	virtual ~SearchForm();
+	bool Initialize(void);
+
+public:
+	virtual result OnInitializing(void);
+	virtual result OnTerminating(void);
+	virtual void OnActionPerformed(const Osp::Ui::Control& source, int actionId);
+};
+
+#endif /* SEARCHFORM_H_ */
---
+++ trunk/DispozPropoz/src/SearchForm.cpp
@@ -0,0 +1,17 @@
+/*
+ * SearchForm.cpp
+ *
+ *  Created on: 15 Oct 2011
+ *      Author: vj
+ */
+
+#include "SearchForm.h"
+
+SearchForm::SearchForm() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SearchForm::~SearchForm() {
+	// TODO Auto-generated destructor stub
+}
