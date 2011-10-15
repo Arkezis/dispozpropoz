3eeb5ed73b1eabfb5526bd93e4780c2423fe983b
sathishvj@gmail.com <sathishvj@gmail.com@5a97ccac-b474-ded1-4a52-e8af14ae9110>
2011-10-15 20:45:38 +0000

    svj: Updates to Search form

--- DispozPropoz/inc/SearchForm.h
+++ DispozPropoz/inc/SearchForm.h
@@ -19,7 +19,8 @@ using namespace Osp::App;
 
 class SearchForm  :
 	public Osp::Ui::Controls::Form,
-	public Osp::Ui::IActionEventListener {
+	public Osp::Ui::IActionEventListener,
+ 	public Osp::Ui::ITouchEventListener {
 
 public:
 	SearchForm();
@@ -30,6 +31,33 @@ public:
 	virtual result OnInitializing(void);
 	virtual result OnTerminating(void);
 	virtual void OnActionPerformed(const Osp::Ui::Control& source, int actionId);
+
+private:
+	static const int ID_SEARCHBAR = 101;
+	static const int ID_CHECKBUTTON_1 = 201;
+	static const int ID_CHECKBUTTON_2 = 202;
+	static const int ID_CHECKBUTTON_3 = 203;
+	static const int ID_CHECKBUTTON_4 = 204;
+	static const int ID_CHECKBUTTON_5 = 205;
+	static const int ID_CHECKBUTTON_6 = 206;
+	static const int ID_BUTTON_SEARCH = 301;
+
+	Osp::Ui::Controls::SearchBar *pSearchBar;
+	Osp::Ui::Controls::CheckButton *pCheckButton1;
+	Osp::Ui::Controls::CheckButton *pCheckButton2;
+	Osp::Ui::Controls::CheckButton *pCheckButton3;
+	Osp::Ui::Controls::CheckButton *pCheckButton4;
+	Osp::Ui::Controls::CheckButton *pCheckButton5;
+	Osp::Ui::Controls::CheckButton *pCheckButton6;
+	Osp::Ui::Controls::FooterItem *pFooterItem;
+
+	virtual void OnTouchDoublePressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchFocusIn(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchFocusOut(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchLongPressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchMoved(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchPressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
+	virtual void OnTouchReleased(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo);
 };
 
 #endif /* SEARCHFORM_H_ */
--- DispozPropoz/src/SearchForm.cpp
+++ DispozPropoz/src/SearchForm.cpp
@@ -42,15 +42,80 @@ SearchForm::OnInitializing(void)
 		__pButtonOk->AddActionEventListener(*this);
 	}
 	*/
+	pSearchBar = static_cast<SearchBar *>(GetControl(L"IDC_SEARCHBAR1"));
+	if (pSearchBar != null)
+	{
+		//pSearchBar->SetActionId(ID_SEARCHBAR);
+		pSearchBar->AddActionEventListener(*this);
+	}
+
+	pCheckButton1 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON1"));
+	if (pCheckButton1 != null)
+	{
+		//pCheckButton1->SetActionId(ID_CHECKBUTTON_1);
+		pCheckButton1->AddActionEventListener(*this);
+	}
+
+	pCheckButton2 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON2"));
+	if (pCheckButton2 != null)
+	{
+		//pCheckButton2->SetActionId(ID_CHECKBUTTON_2);
+		pCheckButton2->AddActionEventListener(*this);
+	}
+
+	pCheckButton3 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON3"));
+	if (pCheckButton3 != null)
+	{
+		//pCheckButton3->SetActionId(ID_CHECKBUTTON_3);
+		pCheckButton3->AddActionEventListener(*this);
+	}
+
+	pCheckButton4 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON4"));
+	if (pCheckButton4 != null)
+	{
+		//pCheckButton4->SetActionId(ID_CHECKBUTTON_4);
+		pCheckButton4->AddActionEventListener(*this);
+	}
+
+	pCheckButton5 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON5"));
+	if (pCheckButton5 != null)
+	{
+		//pCheckButton5->SetActionId(ID_CHECKBUTTON_5);
+		pCheckButton5->AddActionEventListener(*this);
+	}
+
+	pCheckButton6 = static_cast<CheckButton *>(GetControl(L"IDC_CHECKBUTTON6"));
+	if (pCheckButton6 != null)
+	{
+		//pCheckButton6->SetActionId(ID_CHECKBUTTON_6);
+		pCheckButton6->AddActionEventListener(*this);
+	}
 
 	RadioGroup* radioGroupObj = new RadioGroup();
 	radioGroupObj->Construct();
-	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON1")));
+	/*radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON1")));
 	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON2")));
 	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON3")));
 	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON4")));
 	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON5")));
 	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON6")));
+	*/
+
+	radioGroupObj->Add(*pCheckButton1);
+	radioGroupObj->Add(*pCheckButton2);
+	radioGroupObj->Add(*pCheckButton3);
+	radioGroupObj->Add(*pCheckButton4);
+	radioGroupObj->Add(*pCheckButton5);
+	radioGroupObj->Add(*pCheckButton6);
+
+	/* svj: stopping here
+	pFooterItem = static_cast<FooterItem *>(GetControl(L"ID_FOOTERITEM1"));
+	if (pFooterItem != null) {
+		pFooterItem->SetActionId(ID_BUTTON_SEARCH);
+		pFooterItem->AddTouchEventListener(*this);
+		pFooterItem->AddActionEventListener(*this);
+	}
+	*/
 
 	return r;
 }
@@ -79,4 +144,54 @@ SearchForm::OnActionPerformed(const Osp::Ui::Control& source, int actionId)
 	default:
 		break;
 	}
+
+}
+
+void
+SearchForm::OnTouchDoublePressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchFocusIn(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchFocusOut(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchLongPressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchMoved(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchPressed(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
+}
+
+void
+SearchForm::OnTouchReleased(const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo)
+{
+	// TODO: Add your implementation codes here
+
 }
