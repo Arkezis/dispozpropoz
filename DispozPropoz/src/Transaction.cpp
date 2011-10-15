d415ccb8dd1a94f95932dc27e91ed671d13e01ea
sathishvj@gmail.com <sathishvj@gmail.com@5a97ccac-b474-ded1-4a52-e8af14ae9110>
2011-10-15 12:59:57 +0000

    Initial version of Transaction class.

---
+++ DispozPropoz/src/Transaction.cpp
@@ -0,0 +1,17 @@
+/*
+ * Transaction.cpp
+ *
+ *  Created on: 15 Oct 2011
+ *      Author: vj
+ */
+
+#include "Transaction.h"
+
+Transaction::Transaction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Transaction::~Transaction() {
+	// TODO Auto-generated destructor stub
+}
