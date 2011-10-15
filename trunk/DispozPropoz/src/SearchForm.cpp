b31cb2dfc4b0a1b66ce98de4e9da14f8d17ec6ea
sathishvj@gmail.com <sathishvj@gmail.com@5a97ccac-b474-ded1-4a52-e8af14ae9110>
2011-10-15 19:24:07 +0000

    svj: added code and files for search

--- trunk/DispozPropoz/src/SearchForm.cpp
+++ trunk/DispozPropoz/src/SearchForm.cpp
@@ -15,3 +15,68 @@ SearchForm::SearchForm() {
 SearchForm::~SearchForm() {
 	// TODO Auto-generated destructor stub
 }
+
+
+bool
+SearchForm::Initialize()
+{
+	// Construct an XML form
+	Construct(L"SEARCH_FORM");
+
+	return true;
+}
+
+result
+SearchForm::OnInitializing(void)
+{
+	result r = E_SUCCESS;
+
+	// TODO: Add your initialization code here
+
+	// Get a button via resource ID
+	/*
+	__pButtonOk = static_cast<Button *>(GetControl(L"IDC_BUTTON_OK"));
+	if (__pButtonOk != null)
+	{
+		__pButtonOk->SetActionId(ID_BUTTON_OK);
+		__pButtonOk->AddActionEventListener(*this);
+	}
+	*/
+
+	RadioGroup* radioGroupObj = new RadioGroup();
+	radioGroupObj->Construct();
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON1")));
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON2")));
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON3")));
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON4")));
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON5")));
+	radioGroupObj->Add(*static_cast<CheckButton*>(GetControl(L"IDC_CHECKBUTTON6")));
+
+	return r;
+}
+
+result
+SearchForm::OnTerminating(void)
+{
+	result r = E_SUCCESS;
+
+	// TODO: Add your termination code here
+
+	return r;
+}
+
+void
+SearchForm::OnActionPerformed(const Osp::Ui::Control& source, int actionId)
+{
+	switch(actionId)
+	{
+	/*case ID_BUTTON_OK:
+		{
+			AppLog("OK Button is clicked! \n");
+		}
+		break;
+	*/
+	default:
+		break;
+	}
+}
